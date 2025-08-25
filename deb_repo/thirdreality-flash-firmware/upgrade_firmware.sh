462c958f5f057b457aaed30bcca0fc84c222fc23
guoping.liu <guoping.liu@3reality.com>
2025-08-25 02:50:38 +0000

    update blz firmware

--- deb_repo/thirdreality-flash-firmware/upgrade_firmware.sh
+++ deb_repo/thirdreality-flash-firmware/upgrade_firmware.sh
@@ -3,7 +3,7 @@
 # Define source and destination paths
 SRC="/usr/lib/thirdreality/images"
 DST="/lib/firmware/bl706"
-VERSION="1.04.02"
+VERSION="1.08.01"
 WORK_DIR="/mnt/R3Install"
 
 # Function to get current system version components
